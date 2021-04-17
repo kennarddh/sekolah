a3925a13ac3299b9188d57cb180ecf210c62d2aa
kennarddh <kennarddh@gmail.com>
2021-04-17 17:05:11 +0700

    bikin custom module

---
+++ sekolah/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Sekolah',
+    'version': '1.1',
+    'category': 'General',
+    'summary': 'Sekolah',
+    'author':'admin',
+    'description': """
+    ...
+    """,
+    'depends': [],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
